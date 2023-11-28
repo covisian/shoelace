af6b68baf27ff2fb76aec3fd124ff759bfcb02e0
Nicola Pinna <npinna@vivocha.com>
2023-11-28 11:36:39 +0100

    ci: insert commit before tag creation

--- release.js
+++ release.js
@@ -7,16 +7,17 @@ try {
   console.log('Creazione versione');
   execSync('changeset', { stdio: 'inherit' });
   execSync('changeset version', { stdio: 'inherit' });
-  execSync('changeset tag', { stdio: 'inherit' });
-  console.log('Versioni create con successo.');
   try {
     console.log('Committo versione e changelog...');
     execSync('git add package.json CHANGELOG.md', { stdio: 'inherit' });
-    execSync('git push --follow-tags', { stdio: 'inherit' });
+    execSync('git commit -m "fix: publish"', { stdio: 'inherit' });
   } catch (error) {
     'Commit non completato: ', error.message;
   }
+  execSync('changeset tag', { stdio: 'inherit' });
+  console.log('Versioni create con successo.');
 
+  execSync('git push --follow-tags', { stdio: 'inherit' });
   console.log('Versione Creata con successo.');
 } catch (error) {
   console.error("Errore durante l'esecuzione di un comando:", error.message);
